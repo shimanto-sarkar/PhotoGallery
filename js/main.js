ce7ebc28b62a9588971419860d3d5147f9b15488
shimanto-Web-Dev <shimantowebdeveloper@gmail.com>
2025-02-04 03:49:29 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+$('.main').slick({
+    slidesToShow: 1,
+    slidesToScroll:1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    swipe: false,
+    touchMove: false,
+    prevArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-chevron-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-chevron-right"></i></button>',
+    
+  });
+  
+  var filtered = false;
+  
+  $('.js-filter').on('click', function(){
+    if (filtered === false) {
+      $('.main').slick('slickFilter',':even');
+      $(this).text('Unfilter Slides');
+      filtered = true;
+    } else {
+      $('.main').slick('slickUnfilter');
+      $(this).text('Filter Slides');
+      filtered = false;
+    }
+  });
+
+  // ==================== screen zoom disable 
+   // Disable zooming for mobile (touch events)
+   document.addEventListener('touchstart', function (e) {
+    if (e.touches.length > 1) {
+        e.preventDefault();
+    }
+}, { passive: false });
+
+// Disable zooming on desktop (mouse wheel, etc.)
+document.addEventListener('wheel', function (e) {
+  if (e.ctrlKey || e.metaKey) {
+      e.preventDefault();
+  }
+}, { passive: false });

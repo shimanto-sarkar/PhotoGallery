b0c363dce4e0e3305b38ad1faab1b1ee95d981a2
shimanto-Web-Dev <shimantowebdeveloper@gmail.com>
2025-02-04 20:39:28 +0600

    gallery done

--- js/main.js
+++ js/main.js
@@ -39,3 +39,21 @@ document.addEventListener('wheel', function (e) {
       e.preventDefault();
   }
 }, { passive: false });
+
+
+// ============back to top gallaery
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
